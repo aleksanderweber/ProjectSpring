2792da8c7574ae67fa44b1224bb5980fd50996ab
Aleksander Weber <aleksander.weber@icloud.com>
2019-11-05 20:54:22 +0100

    added User class

---
+++ src/main/java/com/github/aleksanderweber/demo/controllers/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.github.aleksanderweber.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @GetMapping
+    public String prepareRegistrationPage() {
+        return"";
+    }
+
+    @PostMapping
+    public String processRegistrationPage() {
+        return "";
+    }
+
+}
